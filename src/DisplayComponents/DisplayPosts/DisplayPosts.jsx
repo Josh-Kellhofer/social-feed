4e79da59c24d2cb18efdea2f9ec93737c8134437
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-03-09 15:07:05 -0800

    Added Some Spacing to Text.

--- src/DisplayComponents/DisplayPosts/DisplayPosts.jsx
+++ src/DisplayComponents/DisplayPosts/DisplayPosts.jsx
@@ -13,7 +13,7 @@ const DisplayPosts = (props) => {
             return (
               <tr>
                 <div className="grid-container">
-                  <div className="grid-item"><h3>Post By:<br></br> {name.listedName}</h3></div>
+                  <div className="grid-item"><h3>Post By:<br></br><br></br> {name.listedName}</h3></div>
                   <div className="grid-item">{name.text}</div>
                   <div ><CustomButton/>
                   </div>
